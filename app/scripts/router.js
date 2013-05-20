19dfc275dd8310b0f7193d302cccaa1b7bd367c7
Delwyn de Villiers <delwyn.d@gmail.com>
2013-05-20 10:14:34 +0100

    cleanup router

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -3,22 +3,22 @@ App.Router.reopen({
 });
 
 App.Router.map(function() {
-  this.resource('month', { path: 'month/:date' });
-});
-
-App.MonthRoute = Ember.Route.extend({
-  model: function(params) {
-    return moment(params.date);
-  },
-
-  setupController: function(controller, model) {
-    controller.set('date', moment(model));
-    controller.set('events', App.Event.find());
-  }
+  this.route('month', { path: 'month/:date' });
 });
 
 App.IndexRoute = Ember.Route.extend({
   redirect: function() {
     this.transitionTo('month', moment().format('YYYY-MM'));
+  }
+});
+
+App.MonthRoute = Ember.Route.extend({
+  model: function(params) {
+    return params.date;
+  },
+
+  setupController: function(controller, model) {
+    controller.set('date', moment(model, 'YYYY-MM'));
+    controller.set('events', App.Event.find());
   }
 });
